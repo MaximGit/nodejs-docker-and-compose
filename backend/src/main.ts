59c18b533f6188197d4909b743bbb423a592fea8
Maxim Tuzikov <mtuzikov@dev.vtb.ru>
2025-06-20 21:29:13 +0700

    fix pm2, add readme

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
